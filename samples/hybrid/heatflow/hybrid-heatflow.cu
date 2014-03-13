23ee2a9f11d2888f061a6eff029abe4fedf8e919
tmquan <tmquan@head.cm.cluster>
2014-03-13 16:03:27 +0900

    More update

--- samples/hybrid/heatflow/hybrid-heatflow.cu
+++ samples/hybrid/heatflow/hybrid-heatflow.cu
@@ -25,6 +25,45 @@ using namespace std;
 	MPI_Barrier(MPI_COMM_WORLD);	                                               					 			\
 }
 // -----------------------------------------------------------------------------------
+/// Mirror effect, acts like Neumann Boundary Condition
+#define at(x, y, z, dimx, dimy, dimz) (clamp(z, 0, dimz-1)*dimy*dimx		\
+									  +clamp(y, 0, dimy-1)*dimx				\
+									  +clamp(x, 0, dimx-1))				
+// -----------------------------------------------------------------------------------
+__global__
+void __heatflow(float *src, float *dst, int dimx, int dimy, int dimz)
+{
+	//3D global index
+	int3 index_3d = make_int3(
+		blockIdx.x*blockDim.x+threadIdx.x,
+		blockIdx.y*blockDim.y+threadIdx.y,
+		blockIdx.z*blockDim.z+threadIdx.z);
+	
+	//Check valid indices
+	if (index_3d.x >= dimx || index_3d.y >= dimy || index_3d.z >= dimz)
+		return;
+	
+	//
+	dst[at(index_3d.x, index_3d.y, index_3d.z, dimx, dimy, dimz)]
+	=  	(src[at(index_3d.x+1, index_3d.y+0, index_3d.z+0, dimx, dimy, dimz)] +
+		 src[at(index_3d.x-1, index_3d.y+0, index_3d.z+0, dimx, dimy, dimz)] +
+		 
+		 src[at(index_3d.x+0, index_3d.y+1, index_3d.z+0, dimx, dimy, dimz)] +
+		 src[at(index_3d.x+0, index_3d.y-1, index_3d.z+0, dimx, dimy, dimz)] +
+		 
+		 src[at(index_3d.x+0, index_3d.y+0, index_3d.z+1, dimx, dimy, dimz)] +
+		 src[at(index_3d.x+0, index_3d.y+0, index_3d.z-1, dimx, dimy, dimz)]) / 6.0f;
+}
+// -----------------------------------------------------------------------------------
+void heatflow(float *src, float *dst, int dimx, int dimy, int dimz)
+{
+	dim3 numBlocks((dimx/8 + ((dimx%8)?1:0)),
+				   (dimy/8 + ((dimy%8)?1:0)),
+				   (dimz/8 + ((dimz%8)?1:0)) );
+	dim3 numThreads(8, 8, 8);
+	__heatflow<<<numBlocks, numThreads>>>(src, dst, dimx, dimy, dimz);
+}
+// -----------------------------------------------------------------------------------
 int main (int argc, char *argv[])
 {
 	
@@ -115,28 +154,34 @@ int main (int argc, char *argv[])
 	// if(rank==master)	cmd.printParams();
 	// if(rank==master)	cout << dimx << endl << dimy << endl << dimz << endl;
 	//================================================================================
-	// Determine main problem size and data partition same as CUDA style
-	dim3 blockDim;
-	dim3 gridDim;
+	//!!! Determine main problem size and data partition same as CUDA style
+	dim3 procDim;
+	dim3 knotDim;
 	dim3 haloDim;
 
-	blockDim.x = dimx;
-	blockDim.y = dimy;
-	blockDim.z = dimz/numWorkers;	// We partition only along z
-	
-	gridDim.x  = dimx/blockDim.x + (dimx%blockDim.x)?1:0;
-	gridDim.y  = dimy/blockDim.y + (dimy%blockDim.y)?1:0;
-	gridDim.z  = dimz/blockDim.z + (dimz%blockDim.z)?1:0;
-	
 	haloDim.x = 0;
 	haloDim.y = 0;
-	haloDim.z = 1;	// Pad 1 
+	haloDim.z = 3;	// Pad 1 
 	
+	procDim.x = dimx;
+	procDim.y = dimy;
+	procDim.z = dimz/numWorkers;	// We partition only along z
+	if(numWorkers==1)
+		procDim.z = dimz/numWorkers;	// We partition only along z
+	else
+	{
+		if(rank==head) 			procDim.z = 1*haloDim.z + dimz/numWorkers + 0*haloDim.z;	// We partition only along z
+		else if(rank==tail) 	procDim.z = 0*haloDim.z + dimz/numWorkers + 1*haloDim.z;	// We partition only along z
+		else					procDim.z = 1*haloDim.z + dimz/numWorkers + 1*haloDim.z;	// We partition only along z
+	}
+	knotDim.x  = dimx/procDim.x + (dimx%procDim.x)?1:0;
+	knotDim.y  = dimy/procDim.y + (dimy%procDim.y)?1:0;
+	knotDim.z  = dimz/procDim.z + (dimz%procDim.z)?1:0;
 	
 	if(rank==head) 
 	{
-		cout << blockDim.x << endl << blockDim.y << endl << blockDim.z << endl;
-		cout << gridDim.x  << endl << gridDim.y  << endl << gridDim.z  << endl;
+		cout << procDim.x << endl << procDim.y << endl << procDim.z << endl;
+		cout << knotDim.x  << endl << knotDim.y  << endl << knotDim.z  << endl;
 	}
 	
 	//================================================================================
@@ -147,8 +192,8 @@ int main (int argc, char *argv[])
 	
 
 	int total 		= dimx * dimy * dimz;
-	int validSize	= blockDim.x * blockDim.y * blockDim.z; // Valid data range
-	int haloSize    = blockDim.x * blockDim.y * haloDim.z;
+	int validSize	= dimx * dimy * dimz / numWorkers; // Valid data range
+	int haloSize    = dimx * dimy * haloDim.z;
 					  
 	MPI_Sync("Allocating total memory at master");
 	if(rank==master)
@@ -173,6 +218,9 @@ int main (int argc, char *argv[])
 	int middleSize  = validSize + 2*haloSize;
 	int tailSize    = validSize + 1*haloSize;
 	
+	int procSize    = procDim.x*procDim.y*procDim.z;
+	
+	
 	float *p_src, *p_dst;
 	p_src = NULL;
 	p_dst = NULL;
@@ -181,14 +229,15 @@ int main (int argc, char *argv[])
 	cout << "Allocating src memory at " << rank << endl;
 	MPI_Sync("");
 	//================================================================================
-	if(numWorkers == 1)
-		p_src = new float[validSize];
-	else
-	{
-		if(rank==head)	 		p_src = new float[headSize];
-		else if (rank==tail)	p_src = new float[tailSize];
-		else 					p_src = new float[middleSize];
-	}
+	p_src = new float[procSize];
+	// if(numWorkers == 1)
+		// p_src = new float[validSize];
+	// else
+	// {
+		// if(rank==head)	 		p_src = new float[headSize];
+		// else if (rank==tail)	p_src = new float[tailSize];
+		// else 					p_src = new float[middleSize];
+	// }
 	//================================================================================
 	MPI_Sync("Done");
 	//================================================================================
@@ -196,14 +245,15 @@ int main (int argc, char *argv[])
 	cout << "Allocating dst memory at " << rank << endl;
 	MPI_Sync("");
 	//================================================================================
-	if(numWorkers == 1)
-		p_dst = new float[validSize];
-	else
-	{
-		if(rank==head)	 		p_dst = new float[headSize];
-		else if (rank==tail)	p_dst = new float[tailSize];
-		else 					p_dst = new float[middleSize];
-	}
+	p_dst = new float[procSize];
+	// if(numWorkers == 1)
+		// p_dst = new float[validSize];
+	// else
+	// {
+		// if(rank==head)	 		p_dst = new float[headSize];
+		// else if (rank==tail)	p_dst = new float[tailSize];
+		// else 					p_dst = new float[middleSize];
+	// }
 	//================================================================================
 	MPI_Sync("");
 	cout << "Allocated  dst memory at " << rank << endl;
@@ -213,16 +263,16 @@ int main (int argc, char *argv[])
 	
 	// Scatter the data
 	MPI_Sync("Master is scattering the data");
-	if(rank==master)	//Send
+	if(rank==master)	//Send 
 	{
 		if(numWorkers==1)
 			MPI_Isend(h_src, validSize, MPI_FLOAT, head, 0, MPI_COMM_WORLD, &request);
 		else
 		{
 			// Send to head
-			MPI_Isend(h_src, 							   	headSize, 	MPI_FLOAT, head, 0, MPI_COMM_WORLD, &request);
+			MPI_Isend(h_src,                                headSize, MPI_FLOAT, head, 0, MPI_COMM_WORLD, &request);
 			// Send to tail
-			MPI_Isend(h_src + tail*validSize - haloSize,    tailSize, 	MPI_FLOAT, tail, 0, MPI_COMM_WORLD, &request);	
+			MPI_Isend(h_src + tail*validSize - haloSize,    tailSize, MPI_FLOAT, tail, 0, MPI_COMM_WORLD, &request);	
 			// Send to middle
 			for(int mid=head+1; mid<tail; mid++)
 				MPI_Isend(h_src + mid*validSize - haloSize, middleSize, MPI_FLOAT, mid, 0, MPI_COMM_WORLD, &request);
@@ -230,29 +280,163 @@ int main (int argc, char *argv[])
 	}
 	
 	// Receive data
-	if(numWorkers==1)
-		MPI_Recv(p_src, validSize, MPI_FLOAT, master, 0, MPI_COMM_WORLD, &status);
-	else
-	{
-		// Send to head
-		if(rank==head) 			MPI_Recv(p_src, headSize,   MPI_FLOAT, master, 0, MPI_COMM_WORLD, &status);
-		else if(rank==tail) 	MPI_Recv(p_src, tailSize,   MPI_FLOAT, master, 0, MPI_COMM_WORLD, &status);
-		else					MPI_Recv(p_src, middleSize, MPI_FLOAT, master, 0, MPI_COMM_WORLD, &status);
-	}
+	MPI_Recv(p_src, procSize, MPI_FLOAT, master, 0, MPI_COMM_WORLD, &status); 
+	// if(numWorkers==1)
+		// MPI_Recv(p_src, validSize, MPI_FLOAT, master, 0, MPI_COMM_WORLD, &status);
+	// else
+	// {
+		// // Send to head
+		// if(rank==head) 		MPI_Recv(p_src, headSize,   MPI_FLOAT, master, 0, MPI_COMM_WORLD, &status);
+		// else if(rank==tail) 	MPI_Recv(p_src, tailSize,   MPI_FLOAT, master, 0, MPI_COMM_WORLD, &status);
+		// else					MPI_Recv(p_src, middleSize, MPI_FLOAT, master, 0, MPI_COMM_WORLD, &status);
+	// }
 	MPI_Sync("Done");
 	//================================================================================
 	// Processing here, assume processed, copy directly form src to dst
+	int numLoops = 20;
 	MPI_Sync("Processing the data");
+	// Common pattern
 	if(numWorkers==1)
-		memcpy(p_dst, p_src, validSize*sizeof(float));
+		; // Adjust the size
 	else
 	{
-		// Send to head
-		if(rank==head) 			memcpy(p_dst, p_src, headSize*sizeof(float));
-		else if(rank==tail) 	memcpy(p_dst, p_src, tailSize*sizeof(float));
-		else					memcpy(p_dst, p_src, middleSize*sizeof(float));
+		if(rank==head) 			; // Adjust the size
+		else if(rank==tail) 	; // Adjust the size
+		else					; // Adjust the size
 	}
+	// if(numWorkers==1)
+		// memcpy(p_dst, p_src, validSize*sizeof(float));
+	// else
+	// {
+		// // Send to head
+		// if(rank==head) 			memcpy(p_dst, p_src, headSize*sizeof(float));
+		// else if(rank==tail) 	memcpy(p_dst, p_src, tailSize*sizeof(float));
+		// else					memcpy(p_dst, p_src, middleSize*sizeof(float));
+	// }
+	
+	// Declare GPU memory
+	float *d_src;
+	cudaMalloc((void**)&d_src, (procSize)*sizeof(float));
+	// if(numWorkers==1)
+		// cudaMalloc((void**)&d_src, (validSize)*sizeof(float));
+	// else
+	// {
+		// if(rank==head) 			cudaMalloc((void**)&d_src, (headSize)*sizeof(float));
+		// else if(rank==tail) 	cudaMalloc((void**)&d_src, (tailSize)*sizeof(float));
+		// else					cudaMalloc((void**)&d_src, (middleSize)*sizeof(float));
+	// }
+	
+	float *d_dst;
+	cudaMalloc((void**)&d_dst, (procSize)*sizeof(float));
+	// if(numWorkers==1)
+		// cudaMalloc((void**)&d_dst, (validSize)*sizeof(float));
+	// else
+	// {
+		// if(rank==head) 		cudaMalloc((void**)&d_dst, (headSize)*sizeof(float));
+		// else if(rank==tail) 	cudaMalloc((void**)&d_dst, (tailSize)*sizeof(float));
+		// else					cudaMalloc((void**)&d_dst, (middleSize)*sizeof(float));
+	// }
+	MPI_Sync("");
+	
+	// Copy to GPU memory
+	cudaMemcpy(d_src, p_src, (procSize)*sizeof(float), cudaMemcpyHostToDevice);
+	// if(numWorkers==1)
+		// cudaMemcpy(d_src, p_src, (validSize)*sizeof(float), cudaMemcpyHostToDevice);
+	// else
+	// {
+		// if(rank==head) 		cudaMemcpy(d_src, p_src, (headSize)*sizeof(float), cudaMemcpyHostToDevice);
+		// else if(rank==tail) 	cudaMemcpy(d_src, p_src, (tailSize)*sizeof(float), cudaMemcpyHostToDevice);
+		// else					cudaMemcpy(d_src, p_src, (middleSize)*sizeof(float), cudaMemcpyHostToDevice);
+	// }
+	MPI_Sync("");
+	
+	// Launch the kernel
+	for(int loop=0; loop<numLoops; loop++)
+	{
+		// Launch the kernel
+		heatflow(d_src, d_dst, procDim.x, procDim.y, procDim.z);
+		// if(numWorkers==1)
+			// heatflow(d_src, d_dst, procDim.x, procDim.y, procDim.z);
+		// else
+		// {
+			// if(rank==head) 		heatflow(d_src, d_dst, procDim.x, procDim.y, procDim.z+1*haloDim.z);
+			// else if(rank==tail) 	heatflow(d_src, d_dst, procDim.x, procDim.y, 1*haloDim.z+procDim.z);
+			// else					heatflow(d_src, d_dst, procDim.x, procDim.y, 1*haloDim.z+procDim.z+1*haloDim.z);
+		// }
+	
+		// Device synchronize
+		cudaDeviceSynchronize();		cudaCheckLastError();
+		MPI_Sync("Device Synchronization");	
+		
+		// Transfer the halo here
+		// Copy to right, tail cannot perform
+		//  // +-+-+---------+-+-+     +-+-+---------+-+-+     +-+-+---------+-+-+
+		// --> |R| | (i,j-1) |S| | --> |R| |  (i,j)  |S| | --> |R| | (i,j+1) |S| | -->
+		//	// +-+-+---------+-+-+     +-+-+---------+-+-+     +-+-+---------+-+-+
+		if(numWorkers==1)
+			; // No need
+		else
+		{
+			if(rank<tail)	MPI_Isend(d_dst + procSize - 2*haloSize, haloSize, MPI_FLOAT, rank+1, 0, MPI_COMM_WORLD, &request);
+			if(rank>head)	MPI_Recv (d_dst, 						 haloSize, MPI_FLOAT, rank-1, 0, MPI_COMM_WORLD, &status);					
+		} 
+		MPI_Sync("Transfer to right");
+		
+		// Copy to left, head cannot perform
+		// // +-+-+---------+-+-+     +-+-+---------+-+-+     +-+-+---------+-+-+
+		//<-- |X|S| (i,j-1) | |R| <-- |X|S|  (i,j)  | |R| <-- |X|S| (i,j+1) | |R| <--
+		// // +-+-+---------+-+-+     +-+-+---------+-+-+     +-+-+---------+-+-+
+		if(numWorkers==1)
+			; // No need
+		else
+		{
+			if(rank>head)	MPI_Isend(d_dst + 1*haloSize, 			 haloSize, MPI_FLOAT, rank-1, 0, MPI_COMM_WORLD, &request);
+			if(rank<tail)	MPI_Recv (d_dst + procSize - 1*haloSize, haloSize, MPI_FLOAT, rank+1, 0, MPI_COMM_WORLD, &status);					
+		} 
+		MPI_Sync("Transfer to left");
+		
+		if(loop==(numLoops-1))	break;
+		std::swap(d_src, d_dst);
+		MPI_Sync("");
+	}
+	
+	// Copy to CPU memory
+	cudaMemcpy(p_dst, d_dst, (procSize)*sizeof(float), cudaMemcpyDeviceToHost);
+	// if(numWorkers==1)
+		// cudaMemcpy(p_dst, d_dst, (validSize)*sizeof(float), cudaMemcpyDeviceToHost);
+	// else
+	// {
+		// if(rank==head) 		cudaMemcpy(p_dst, d_dst, (headSize)*sizeof(float), cudaMemcpyDeviceToHost);
+		// else if(rank==tail) 	cudaMemcpy(p_dst, d_dst, (tailSize)*sizeof(float), cudaMemcpyDeviceToHost);
+		// else					cudaMemcpy(p_dst, d_dst, (middleSize)*sizeof(float), cudaMemcpyDeviceToHost);
+	// }
+	MPI_Sync("");
 	MPI_Sync("Done");
+	//================================================================================
+	// Calculate the golden result
+	float *h_src_ref = NULL;
+	float *h_dst_ref = NULL;
+	if(rank==master)	
+	{
+		h_src_ref = new float[total];
+		h_dst_ref = new float[total];
+		memcpy(h_src_ref, h_src, total*sizeof(float));
+		
+		for(int loop=0; loop<numLoops; loop++)
+		{
+			for(int z=0; z<dimz; z++)
+				for(int y=0; y<dimy; y++)
+					for(int x=0; x<dimx; x++)
+						h_dst_ref[at(x, y, z, dimx, dimy, dimz)] =	(h_src_ref[at(x+1, y+0, z+0, dimx, dimy, dimz)] +
+																	 h_src_ref[at(x-1, y+0, z+0, dimx, dimy, dimz)] +
+																	 h_src_ref[at(x+0, y+1, z+0, dimx, dimy, dimz)] +
+																	 h_src_ref[at(x+0, y-1, z+0, dimx, dimy, dimz)] +
+																	 h_src_ref[at(x+0, y+0, z+1, dimx, dimy, dimz)] +
+																	 h_src_ref[at(x+0, y+0, z-1, dimx, dimy, dimz)]) /6.0f;
+			if(loop==(numLoops-1))	break;
+			std::swap(h_src_ref, h_dst_ref);
+		}
+	}
 	//================================================================================
 	// Gathering the data
 	MPI_Sync("Master is gathering the data");
@@ -262,9 +446,9 @@ int main (int argc, char *argv[])
 	else
 	{
 		// Send to head
-		if(rank==head) 			MPI_Isend(p_src, 				validSize, MPI_FLOAT, master, 0, MPI_COMM_WORLD, &request);
-		else if(rank==tail) 	MPI_Isend(p_src + haloSize, 	validSize, MPI_FLOAT, master, 0, MPI_COMM_WORLD, &request);
-		else					MPI_Isend(p_src + haloSize, 	validSize, MPI_FLOAT, master, 0, MPI_COMM_WORLD, &request);
+		if(rank==head) 			MPI_Isend(p_dst, 				validSize, MPI_FLOAT, master, 0, MPI_COMM_WORLD, &request);
+		else if(rank==tail) 	MPI_Isend(p_dst + haloSize, 	validSize, MPI_FLOAT, master, 0, MPI_COMM_WORLD, &request);
+		else					MPI_Isend(p_dst + haloSize, 	validSize, MPI_FLOAT, master, 0, MPI_COMM_WORLD, &request);
 	}
 	/// Receive data
 	if(rank==master)
@@ -290,7 +474,7 @@ int main (int argc, char *argv[])
 	{
 		for(int k=0; k<total; k++)
 		{
-			if(h_src[k] != h_dst[k])
+			if(h_dst_ref[k] != h_dst[k])
 			{
 				cout << "Do not match at " << k << endl;
 				goto cleanup;
