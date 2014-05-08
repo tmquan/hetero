f4a6c5ff832305659312a66eccf72e8b715bd33d
tmquan <tmquan@head.cm.cluster>
2014-05-08 18:04:32 +0900

    Sweep read.write 59 GFLOPS

--- samples/ldav14/debug_stencil_3d/stencil_3d.cu
+++ samples/ldav14/debug_stencil_3d/stencil_3d.cu
@@ -34,14 +34,13 @@ __global__
 void __stencil_3d(float* deviceSrc, float* deviceDst, int dimx, int dimy, int dimz, int halo)
 {
     extern __shared__ float sharedMemSrc[];                     										
-    int  shared_index_1d, global_index_1d, index_1d;                                      										
-    int3 shared_index_3d, global_index_3d, index_3d;                                      										
+    int  closed_index_1d, opened_index_1d, global_index_1d, index_1d;                                      										
+    int3 closed_index_3d, opened_index_3d, global_index_3d, index_3d;
+	
 	int3 sharedMemDim;
 	int sharedMemSize, blockSize, numBatches, numReading, numWorking;
-	int closed_index_1d;
-	int3 closed_index_3d;
-	float center;
 	
+	float center;	
 	float result;
 	int batch, pass, h;
 	
@@ -72,26 +71,26 @@ void __stencil_3d(float* deviceSrc, float* deviceDst, int dimx, int dimy, int di
 	{
 		for(batch=0; batch<numReading; batch++)                                           										
 		{                                                                                     										
-			shared_index_1d  =  threadIdx.z * blockDim.y * blockDim.x+                      										
+			opened_index_1d  =  threadIdx.z * blockDim.y * blockDim.x+                      										
 								threadIdx.y * blockDim.x +                                   										
 								threadIdx.x+                                                										
 								blockDim.x*blockDim.y*blockDim.z*batch; //Magic is here quantm@unist.ac.kr           										
-			shared_index_3d  =  make_int3((shared_index_1d % ((blockDim.y+2*halo)*(2*blockDim.x+2*halo))) % (2*blockDim.x+2*halo),		
-										  (shared_index_1d % ((blockDim.y+2*halo)*(2*blockDim.x+2*halo))) / (2*blockDim.x+2*halo),		
-										  (shared_index_1d / ((blockDim.y+2*halo)*(2*blockDim.x+2*halo))) );      					
-			global_index_3d  =  make_int3(blockIdx.x *2*blockDim.x+shared_index_3d.x-halo, 										
-										  blockIdx.y *  blockDim.y+shared_index_3d.y-halo, 										
-										  blockIdx.z *  blockDim.z+shared_index_3d.z-halo);										
+			opened_index_3d  =  make_int3((opened_index_1d % ((blockDim.y+2*halo)*(2*blockDim.x+2*halo))) % (2*blockDim.x+2*halo),		
+										  (opened_index_1d % ((blockDim.y+2*halo)*(2*blockDim.x+2*halo))) / (2*blockDim.x+2*halo),		
+										  (opened_index_1d / ((blockDim.y+2*halo)*(2*blockDim.x+2*halo))) );      					
+			global_index_3d  =  make_int3(blockIdx.x *2*blockDim.x+opened_index_3d.x-halo, 										
+										  blockIdx.y *  blockDim.y+opened_index_3d.y-halo, 										
+										  blockIdx.z *  blockDim.z+opened_index_3d.z-halo);										
 			global_index_1d  =  global_index_3d.z * dimy * dimx+                                   								
 								global_index_3d.y * dimx+                                   										
 								global_index_3d.x;                                            										
-			if (shared_index_3d.z < (blockDim.z+2*halo))                                    										
+			if (opened_index_3d.z < (blockDim.z+2*halo))                                    										
 			{                                                                                 										
 				if(global_index_3d.z >= 0 && global_index_3d.z < dimz &&                      										
 				   global_index_3d.y >= 0 && global_index_3d.y < dimy &&                        									
 				   global_index_3d.x >= 0 && global_index_3d.x < dimx)                        										
 				{                                                                             										
-					sharedMemSrc[at(shared_index_3d.x, shared_index_3d.y, shared_index_3d.z, 
+					sharedMemSrc[at(opened_index_3d.x, opened_index_3d.y, opened_index_3d.z, 
 								    sharedMemDim.x, sharedMemDim.y, sharedMemDim.z)] 
 					= deviceSrc[global_index_1d];                         
 				}                                                                             
@@ -99,8 +98,7 @@ void __stencil_3d(float* deviceSrc, float* deviceDst, int dimx, int dimy, int di
 		}                                                                                     
 		__syncthreads();                                                                  
 																							  
-		// Stencil  processing here    
-		// nx times write 
+		// Stencil  processing in here    
 		for(batch=0; batch<numWorking; batch++)
 		{
 			closed_index_1d  =  threadIdx.z * blockDim.y * blockDim.x+                      										
@@ -119,111 +117,85 @@ void __stencil_3d(float* deviceSrc, float* deviceDst, int dimx, int dimy, int di
 			result  = sharedMemSrc[at(closed_index_3d.x+halo+0, closed_index_3d.y+halo+0, closed_index_3d.z+halo+0, 
 									  sharedMemDim.x, sharedMemDim.y, sharedMemDim.z)];	
           
-			// shared_index_1d  =  threadIdx.z * blockDim.y * blockDim.x+                      										
-								// threadIdx.y * blockDim.x +                                   										
-								// threadIdx.x+                                                										
-								// blockDim.x*blockDim.y*blockDim.z*batch; //Magic is here quantm@unist.ac.kr           										
-								
-			// shared_index_3d  =  make_int3((shared_index_1d % ((blockDim.y+2*halo)*(2*blockDim.x+2*halo))) % (2*blockDim.x+2*halo),		
-										  // (shared_index_1d % ((blockDim.y+2*halo)*(2*blockDim.x+2*halo))) / (2*blockDim.x+2*halo),		
-										  // (shared_index_1d / ((blockDim.y+2*halo)*(2*blockDim.x+2*halo))) );      					
-			// global_index_3d  =  make_int3(blockIdx.x *2*blockDim.x+shared_index_3d.x-halo, 										
-										  // blockIdx.y *  blockDim.y+shared_index_3d.y-halo, 										
-										  // blockIdx.z *  blockDim.z+shared_index_3d.z-halo);										
-			// global_index_1d  =  global_index_3d.z * dimy * dimx+                                   								
-								// global_index_3d.y * dimx+                                   										
-								// global_index_3d.x;        
-			// result  = sharedMemSrc[at(shared_index_3d.x+halo+0, shared_index_3d.y+halo+0, shared_index_3d.z+halo+0, 
-									  // sharedMemDim.x, sharedMemDim.y, sharedMemDim.z)];	
-
-																							   
-			// // Single pass writing here                                                           
-			// index_3d       =  make_int3(blockIdx.x * blockDim.x+threadIdx.x,                    
-										// blockIdx.y * blockDim.y+threadIdx.y,                    
-										// blockIdx.z * blockDim.z+threadIdx.z);                   
-			// index_1d       =  index_3d.z * dimy * dimx+                                         
-							  // index_3d.y * dimx+                                                
-							  // index_3d.x;                                                         
-			// if (shared_index_3d.z < (blockDim.z+2*halo))                                    										
-			// {                                                                                 										
+			                            
+			if (closed_index_3d.z < (blockDim.z))                                    										
+			{                                                                                 										
 				if(global_index_3d.z >= 0 && global_index_3d.z < dimz &&                      										
 				   global_index_3d.y >= 0 && global_index_3d.y < dimy &&                        									
 				   global_index_3d.x >= 0 && global_index_3d.x < dimx)                        										
 				{  																				   																   
 					deviceDst[global_index_1d] = result;                                        
 				}
-			// }
+			}
 		}
 	}
 	
-	
-	// index_3d       =  make_int3(blockIdx.x * blockDim.x*2+threadIdx.x+blockDim.x,                    
-								// blockIdx.y * blockDim.y+threadIdx.y,                    
-								// blockIdx.z * blockDim.z+threadIdx.z);                   
-	// index_1d       =  index_3d.z * dimy * dimx+                                         
-					  // index_3d.y * dimx+                                                
-					  // index_3d.x;           
-	// if((index_3d.x >= dimx)||(index_3d.y>=dimy)||(index_3d.z>dimz))
-		// return;
-		
-	 // // Multi batch reading here                                                           										
-    // sharedMemDim    = make_int3(blockDim.x*2+2*halo+blockDim.x,                                   										
-                                // blockDim.y+2*halo,                                  										
-                                // blockDim.z+2*halo);                                  										
-    // sharedMemSize   = sharedMemDim.x*sharedMemDim.y*sharedMemDim.z;                                    										
-    // blockSize        = blockDim.x*blockDim.y*blockDim.z*2;                  									
-    // numBatches       = sharedMemSize/blockSize+((sharedMemSize%blockSize)?1:0);    
-
-
-
-	// //First pass will load entire 3 planes, process and write
-	// {
-		// for(batch=0; batch<numBatches; batch++)                                           										
-		// {                                                                                     										
-			// shared_index_1d  =  threadIdx.z * blockDim.y * blockDim.x*2+                      										
-								// threadIdx.y * blockDim.x*2+                                   										
-								// threadIdx.x + blockDim.x +                                              										
-								// blockSize*batch; //Magic is here quantm@unist.ac.kr           										
-			// shared_index_3d  =  make_int3((shared_index_1d % ((blockDim.y+2*halo)*(blockDim.x*2+2*halo))) % (blockDim.x*2+2*halo),		
-										  // (shared_index_1d % ((blockDim.y+2*halo)*(blockDim.x*2+2*halo))) / (blockDim.x*2+2*halo),		
-										  // (shared_index_1d / ((blockDim.y+2*halo)*(blockDim.x*2+2*halo))) );      					
-			// global_index_3d  =  make_int3(blockIdx.x * blockDim.x*2+shared_index_3d.x-halo, 										
-										  // blockIdx.y * blockDim.y+shared_index_3d.y-halo, 										
-										  // blockIdx.z * blockDim.z+shared_index_3d.z-halo);										
-			// global_index_1d  =  global_index_3d.z * dimy * dimx+                                   								
-								// global_index_3d.y * dimx+                                   										
-								// global_index_3d.x;                                            										
-			// if (shared_index_3d.z < (blockDim.z+2*halo))                                    										
-			// {                                                                                 										
-				// if(global_index_3d.z >= 0 && global_index_3d.z < dimz &&                      										
-				   // global_index_3d.y >= 0 && global_index_3d.y < dimy &&                        									
-				   // global_index_3d.x >= 0 && global_index_3d.x < dimx)                        										
-				// {                                                                             										
-					// sharedMemSrc[at(shared_index_3d.x, shared_index_3d.y, shared_index_3d.z, 
-								    // sharedMemDim.x, sharedMemDim.y, sharedMemDim.z)] 
-					// = deviceSrc[global_index_1d];                         
-				// }                                                                             
-			// }                                                                                 
-		// }                                                                                     
-		// __syncthreads();                                                                  
+	// Next marching
+	for(pass=halo; pass<dimz; pass++)
+	{
+		//Shift the plane
+		center = sharedMemSrc[at(threadIdx.x+halo, threadIdx.y+halo, threadIdx.z+halo+halo,    sharedMemDim.x, sharedMemDim.y, sharedMemDim.z)];
+		for(batch=0; batch<numReading; batch++)                                           										
+		{                                                                                     										
+			opened_index_1d  =  threadIdx.z * blockDim.y * blockDim.x+                      										
+								threadIdx.y * blockDim.x +                                   										
+								threadIdx.x+                                                										
+								blockDim.x*blockDim.y*blockDim.z*batch; //Magic is here quantm@unist.ac.kr           										
+			opened_index_3d  =  make_int3((opened_index_1d % ((blockDim.y+2*halo)*(2*blockDim.x+2*halo))) % (2*blockDim.x+2*halo),		
+										  (opened_index_1d % ((blockDim.y+2*halo)*(2*blockDim.x+2*halo))) / (2*blockDim.x+2*halo),		
+										  (opened_index_1d / ((blockDim.y+2*halo)*(2*blockDim.x+2*halo))) );      					
+			global_index_3d  =  make_int3(blockIdx.x *2*blockDim.x+opened_index_3d.x-halo, 										
+										  blockIdx.y *  blockDim.y+opened_index_3d.y-halo, 										
+										  pass 		 *  blockDim.z+opened_index_3d.z-halo);										
+			global_index_1d  =  global_index_3d.z * dimy * dimx+                                   								
+								global_index_3d.y * dimx+                                   										
+								global_index_3d.x;                                            										
+			if (opened_index_3d.z < (blockDim.z+2*halo))                                    										
+			{                                                                                 										
+				if(global_index_3d.z >= 0 && global_index_3d.z < dimz &&                      										
+				   global_index_3d.y >= 0 && global_index_3d.y < dimy &&                        									
+				   global_index_3d.x >= 0 && global_index_3d.x < dimx)                        										
+				{                                                                             										
+					sharedMemSrc[at(opened_index_3d.x, opened_index_3d.y, opened_index_3d.z, 
+								    sharedMemDim.x, sharedMemDim.y, sharedMemDim.z)] 
+					= deviceSrc[global_index_1d];                         
+				}                                                                             
+			}                                                                                 
+		}                                                                                     
+		__syncthreads();                                                                  
 																							  
-		// // Stencil  processing here                                                           
-		// result  = sharedMemSrc[at(threadIdx.x+halo+0, threadIdx.y+halo+0, threadIdx.z+halo+0, sharedMemDim.x, sharedMemDim.y, sharedMemDim.z)];	
-
-																							   
-		// // Single pass writing here                                                           
-		// index_3d       =  make_int3(blockIdx.x * blockDim.x*2+threadIdx.x,                    
-									// blockIdx.y * blockDim.y+threadIdx.y,                    
-									// blockIdx.z * blockDim.z+threadIdx.z);                   
-		// index_1d       =  index_3d.z * dimy * dimx+                                         
-						  // index_3d.y * dimx+                                                
-						  // index_3d.x;                                                         
-																							   
-                                                                                   
-		// deviceDst[index_1d] = result;                                        
-	// }
+		// Stencil  processing in here    
+		for(batch=0; batch<numWorking; batch++)
+		{
+			closed_index_1d  =  threadIdx.z * blockDim.y * blockDim.x+                      										
+								threadIdx.y * blockDim.x +                                   										
+								threadIdx.x+                                                										
+								blockDim.x*blockDim.y*blockDim.z*batch; //Magic is here quantm@unist.ac.kr           										
+			closed_index_3d  =  make_int3((closed_index_1d % ((blockDim.y)*(2*blockDim.x))) % (2*blockDim.x),		
+										  (closed_index_1d % ((blockDim.y)*(2*blockDim.x))) / (2*blockDim.x),		
+										  (closed_index_1d / ((blockDim.y)*(2*blockDim.x))) );      					
+			global_index_3d  =  make_int3(blockIdx.x *2*blockDim.x+closed_index_3d.x, 										
+										  blockIdx.y *  blockDim.y+closed_index_3d.y, 										
+										  pass       *  blockDim.z+closed_index_3d.z);										
+			global_index_1d  =  global_index_3d.z * dimy * dimx+                                   								
+								global_index_3d.y * dimx+                                   										
+								global_index_3d.x; 
+			result  = sharedMemSrc[at(closed_index_3d.x+halo+0, closed_index_3d.y+halo+0, closed_index_3d.z+halo+0, 
+									  sharedMemDim.x, sharedMemDim.y, sharedMemDim.z)];	
+          
+			                            
+			if (closed_index_3d.z < (blockDim.z))                                    										
+			{                                                                                 										
+				if(global_index_3d.z >= 0 && global_index_3d.z < dimz &&                      										
+				   global_index_3d.y >= 0 && global_index_3d.y < dimy &&                        									
+				   global_index_3d.x >= 0 && global_index_3d.x < dimx)                        										
+				{  																				   																   
+					deviceDst[global_index_1d] = result;                                        
+				}
+			}
+		}
+	}
 	
-
 
 
 
