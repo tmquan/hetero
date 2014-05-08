596c26201adc9dfc9c82e8e565a5bf2dba6aeb36
tmquan <tmquan@head.cm.cluster>
2014-05-08 23:17:35 +0900

    Sweep plane Read and Write 72 GFLOPS

--- samples/ldav14/debug_stencil_3d/stencil_3d.cu
+++ samples/ldav14/debug_stencil_3d/stencil_3d.cu
@@ -45,14 +45,14 @@ void __stencil_3d(float* deviceSrc, float* deviceDst, int dimx, int dimy, int di
 	int batch, pass, h;
 	
 	///
-	index_3d       =  make_int3(blockIdx.x * blockDim.x+threadIdx.x,                    
-								blockIdx.y * blockDim.y+threadIdx.y,                    
-								blockIdx.z * blockDim.z+threadIdx.z);                   
-	index_1d       =  index_3d.z * dimy * dimx+                                         
-					  index_3d.y * dimx+                                                
-					  index_3d.x;           
-	if((index_3d.x >= dimx)||(index_3d.y>=dimy)||(index_3d.z>dimz))
-		return;
+	// index_3d       =  make_int3(blockIdx.x * blockDim.x+threadIdx.x,                    
+								// blockIdx.y * blockDim.y+threadIdx.y,                    
+								// blockIdx.z * blockDim.z+threadIdx.z);                   
+	// index_1d       =  index_3d.z * dimy * dimx+                                         
+					  // index_3d.y * dimx+                                                
+					  // index_3d.x;           
+	// if((index_3d.x >= dimx)||(index_3d.y>=dimy)||(index_3d.z>dimz))
+		// return;
 		
 	
     // Multi batch reading here                                                           										
@@ -69,6 +69,7 @@ void __stencil_3d(float* deviceSrc, float* deviceDst, int dimx, int dimy, int di
 
 	//First pass will load entire 3 planes, process and write
 	{
+		#pragma unroll
 		for(batch=0; batch<numReading; batch++)                                           										
 		{                                                                                     										
 			opened_index_1d  =  threadIdx.z * blockDim.y * blockDim.x+                      										
@@ -99,6 +100,7 @@ void __stencil_3d(float* deviceSrc, float* deviceDst, int dimx, int dimy, int di
 		__syncthreads();                                                                  
 																							  
 		// Stencil  processing in here    
+		#pragma unroll
 		for(batch=0; batch<numWorking; batch++)
 		{
 			closed_index_1d  =  threadIdx.z * blockDim.y * blockDim.x+                      										
@@ -134,37 +136,8 @@ void __stencil_3d(float* deviceSrc, float* deviceDst, int dimx, int dimy, int di
 	for(pass=halo; pass<dimz; pass++)
 	{
 		//Shift the plane
-		center = sharedMemSrc[at(threadIdx.x+halo, threadIdx.y+halo, threadIdx.z+halo+halo,    sharedMemDim.x, sharedMemDim.y, sharedMemDim.z)];
-		for(batch=0; batch<numReading; batch++)                                           										
-		{                                                                                     										
-			opened_index_1d  =  threadIdx.z * blockDim.y * blockDim.x+                      										
-								threadIdx.y * blockDim.x +                                   										
-								threadIdx.x+                                                										
-								blockDim.x*blockDim.y*blockDim.z*batch; //Magic is here quantm@unist.ac.kr           										
-			opened_index_3d  =  make_int3((opened_index_1d % ((blockDim.y+2*halo)*(2*blockDim.x+2*halo))) % (2*blockDim.x+2*halo),		
-										  (opened_index_1d % ((blockDim.y+2*halo)*(2*blockDim.x+2*halo))) / (2*blockDim.x+2*halo),		
-										  (opened_index_1d / ((blockDim.y+2*halo)*(2*blockDim.x+2*halo))) );      					
-			global_index_3d  =  make_int3(blockIdx.x *2*blockDim.x+opened_index_3d.x-halo, 										
-										  blockIdx.y *  blockDim.y+opened_index_3d.y-halo, 										
-										  pass 		 *  blockDim.z+opened_index_3d.z-halo);										
-			global_index_1d  =  global_index_3d.z * dimy * dimx+                                   								
-								global_index_3d.y * dimx+                                   										
-								global_index_3d.x;                                            										
-			if (opened_index_3d.z < (blockDim.z+2*halo))                                    										
-			{                                                                                 										
-				if(global_index_3d.z >= 0 && global_index_3d.z < dimz &&                      										
-				   global_index_3d.y >= 0 && global_index_3d.y < dimy &&                        									
-				   global_index_3d.x >= 0 && global_index_3d.x < dimx)                        										
-				{                                                                             										
-					sharedMemSrc[at(opened_index_3d.x, opened_index_3d.y, opened_index_3d.z, 
-								    sharedMemDim.x, sharedMemDim.y, sharedMemDim.z)] 
-					= deviceSrc[global_index_1d];                         
-				}                                                                             
-			}                                                                                 
-		}                                                                                     
-		__syncthreads();                                                                  
-																							  
-		// Stencil  processing in here    
+		// Stencil  processing in here  
+		#pragma unroll
 		for(batch=0; batch<numWorking; batch++)
 		{
 			closed_index_1d  =  threadIdx.z * blockDim.y * blockDim.x+                      										
@@ -180,20 +153,87 @@ void __stencil_3d(float* deviceSrc, float* deviceDst, int dimx, int dimy, int di
 			global_index_1d  =  global_index_3d.z * dimy * dimx+                                   								
 								global_index_3d.y * dimx+                                   										
 								global_index_3d.x; 
-			result  = sharedMemSrc[at(closed_index_3d.x+halo+0, closed_index_3d.y+halo+0, closed_index_3d.z+halo+0, 
-									  sharedMemDim.x, sharedMemDim.y, sharedMemDim.z)];	
-          
-			                            
+			
+			center = sharedMemSrc[at(closed_index_3d.x+halo, 
+									 closed_index_3d.y+halo, 
+									 closed_index_3d.z+halo+halo,    
+									 sharedMemDim.x, sharedMemDim.y, sharedMemDim.z)];
 			if (closed_index_3d.z < (blockDim.z))                                    										
 			{                                                                                 										
 				if(global_index_3d.z >= 0 && global_index_3d.z < dimz &&                      										
 				   global_index_3d.y >= 0 && global_index_3d.y < dimy &&                        									
 				   global_index_3d.x >= 0 && global_index_3d.x < dimx)                        										
-				{  																				   																   
-					deviceDst[global_index_1d] = result;                                        
+				{
+					// deviceDst[global_index_1d] = result;                                        
+					deviceDst[global_index_1d] = center;                                        
 				}
 			}
 		}
+		
+		
+		#pragma unroll
+		for(batch=0; batch<numReading; batch++)                                           										
+		{                                                                                     										
+			opened_index_1d  =  threadIdx.z * blockDim.y * blockDim.x+                      										
+								threadIdx.y * blockDim.x +                                   										
+								threadIdx.x+                                                										
+								blockDim.x*blockDim.y*blockDim.z*batch; //Magic is here quantm@unist.ac.kr           										
+			opened_index_3d  =  make_int3((opened_index_1d % ((blockDim.y+2*halo)*(2*blockDim.x+2*halo))) % (2*blockDim.x+2*halo),
+										  (opened_index_1d % ((blockDim.y+2*halo)*(2*blockDim.x+2*halo))) / (2*blockDim.x+2*halo),
+										  (opened_index_1d / ((blockDim.y+2*halo)*(2*blockDim.x+2*halo))) );      					
+			global_index_3d  =  make_int3(blockIdx.x *2*blockDim.x+opened_index_3d.x-halo, 										
+										  blockIdx.y *  blockDim.y+opened_index_3d.y-halo, 										
+										  pass 		 *  blockDim.z+opened_index_3d.z-halo);										
+			global_index_1d  =  global_index_3d.z * dimy * dimx+                                   								
+								global_index_3d.y * dimx+                                   										
+								global_index_3d.x;                                            										
+			if ((opened_index_3d.z < (blockDim.z+2*halo)) && (opened_index_3d.z > halo))							
+			{                                                                                 										
+				if(global_index_3d.z >= 0 && global_index_3d.z < dimz &&                      										
+				   global_index_3d.y >= 0 && global_index_3d.y < dimy &&                        									
+				   global_index_3d.x >= 0 && global_index_3d.x < dimx)                        										
+				{                                                                             										
+					sharedMemSrc[at(opened_index_3d.x, opened_index_3d.y, opened_index_3d.z, 
+								    sharedMemDim.x, sharedMemDim.y, sharedMemDim.z)] 
+					= deviceSrc[global_index_1d];                         
+				}                                                                             
+			}                                                                                 
+		}                                                                                     
+		__syncthreads();                                                                  
+																							  
+		// // Stencil  processing in here  
+		// #pragma unroll
+		// for(batch=0; batch<numWorking; batch++)
+		// {
+			// closed_index_1d  =  threadIdx.z * blockDim.y * blockDim.x+                      										
+								// threadIdx.y * blockDim.x +                                   										
+								// threadIdx.x+                                                										
+								// blockDim.x*blockDim.y*blockDim.z*batch; //Magic is here quantm@unist.ac.kr           										
+			// closed_index_3d  =  make_int3((closed_index_1d % ((blockDim.y)*(2*blockDim.x))) % (2*blockDim.x),		
+										  // (closed_index_1d % ((blockDim.y)*(2*blockDim.x))) / (2*blockDim.x),		
+										  // (closed_index_1d / ((blockDim.y)*(2*blockDim.x))) );      					
+			// global_index_3d  =  make_int3(blockIdx.x *2*blockDim.x+closed_index_3d.x, 										
+										  // blockIdx.y *  blockDim.y+closed_index_3d.y, 										
+										  // pass       *  blockDim.z+closed_index_3d.z);										
+			// global_index_1d  =  global_index_3d.z * dimy * dimx+                                   								
+								// global_index_3d.y * dimx+                                   										
+								// global_index_3d.x; 
+			// // result  = sharedMemSrc[at(closed_index_3d.x+halo+0, closed_index_3d.y+halo+0, closed_index_3d.z+halo+0, 
+									  // // sharedMemDim.x, sharedMemDim.y, sharedMemDim.z)];	
+			// // result = center;
+			                            
+			
+			// if (closed_index_3d.z < (blockDim.z))                                    										
+			// {                                                                                 										
+				// if(global_index_3d.z >= 0 && global_index_3d.z < dimz &&                      										
+				   // global_index_3d.y >= 0 && global_index_3d.y < dimy &&                        									
+				   // global_index_3d.x >= 0 && global_index_3d.x < dimx)                        										
+				// {
+					// // deviceDst[global_index_1d] = result;                                        
+					// deviceDst[global_index_1d] = center;                                        
+				// }
+			// }
+		// }
 	}
 	
 
